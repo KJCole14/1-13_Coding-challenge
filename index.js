2574a10ceeb42a0dfb51433c5fbe0ecd6d576d51
KJCole14 <kabahjcole@gmail.com>
2023-01-13 18:34:25 -0500

    final commit

--- index.js
+++ index.js
@@ -7,7 +7,17 @@ var connection = MySQL.createConnection({
     password: 'Saint-14-2',
     database: 'classicmodels'
 });
-var rows = connection.query('SELECT * FROM customers')[0];
-console.log(rows);
+//C
+//var C = connection.query('CREATE TABLE Test (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, Message VARCHAR(100))')[0];
+//console.log(C);
+// R
+var R = connection.query('SELECT * FROM Test')[0];
+console.log(R);
+// U
+var U = connection.query("INSERT INTO Test(message) VALUE('Hello there!')")[0];
+console.log(U);
+//D
+var D = connection.query('DROP TABLE Test');
+console.log(D);
 // closes the connection when finished
 //connection.close();
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+
 //needed to install type definitions for node "npm i --save-dev @types/node"           
            //   vvv
 const MySQL = require('mysql');
@@ -8,9 +9,20 @@ const connection = MySQL.createConnection({
     password: 'Saint-14-2',
     database: 'classicmodels'
 });
-  
-const [rows] =  connection.query('SELECT * FROM customers');
-console.log(rows);
+//C
+const [C] = connection.query('CREATE TABLE Test (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, Message VARCHAR(100))');
+console.log(C);
 
+// R
+const [R] =  connection.query('SELECT * FROM Test');
+console.log(R);
+
+// U
+const [U] =  connection.query("INSERT INTO Test(message) VALUE('Hello there!')");
+console.log(U);
+
+//D
+var D = connection.query('DROP TABLE Test');
+console.log(D);
 // closes the connection when finished
 //connection.close();
